9a1e6a3ebfdd9dc7767f543befc154e19435effc
david <egbunumalikdavid@gmail.com>
2025-05-28 06:47:47 +0200

    Create JReportorderService.java

---
+++ src/main/java/com/expensetracker/expensetracker/services/JReportorderService.java
@@ -0,0 +1,46 @@
+package com.expensetracker.expensetracker.services;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
+
+
+
+import com.expensetracker.expensetracker.models.Order;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import jakarta.servlet.http.HttpServletResponse;
+@Service
+public class JReportorderService {
+ 
+    @Autowired
+    private OrderRepository orderrrepository;
+ 
+ 
+    public void exportJasperReport(HttpServletResponse response) throws JRException, IOException {
+        List<Order> order = orderrrepository.findAll();
+        //Get file and compile it
+        File file = ResourceUtils.getFile("classpath:orders.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(file.getAbsolutePath());
+        JRBeanCollectionDataSource dataSource = new JRBeanCollectionDataSource(order);
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put("createdBy", "Simplifying Tech");
+        //Fill Jasper report
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, dataSource);
+        //Export report
+        JasperExportManager.exportReportToPdfStream(jasperPrint,response.getOutputStream());
+    }
+}
