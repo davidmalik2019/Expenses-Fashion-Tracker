93484ddd2bb93a20edb01a2c05094f5e4623d3b8
davidmalik2019 <egbunumalikdavid@gmail.com>
2025-06-26 12:13:04 +0100

    Update ExpensetrackerApplication.java

--- src/main/java/com/expensetracker/expensetracker/ExpensetrackerApplication.java
+++ src/main/java/com/expensetracker/expensetracker/ExpensetrackerApplication.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.expensetracker.expensetracker.services.ExportPdf;
+
 import com.expensetracker.expensetracker.models.Order;
 
 import com.expensetracker.expensetracker.services.CustomerreportService;
